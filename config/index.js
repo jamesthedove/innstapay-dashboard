dcaa80683ff3a2a93537693ef3b5df6edfd7ab4d
James Falade <faladeveloper@gmail.com>
2018-10-27 16:30:43 +0100

    disable source maps

--- config/index.js
+++ config/index.js
@@ -56,9 +56,8 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
